ae7be68c23369b992aa1ba0a2b0fe1652f768943
Paulo Andrade <pcdajr@gmail.com>
2018-12-07 22:46:07 -0300

    Tela de filtro por preço
    Construção da tela de pesquisa do Cliente

---
+++ app/src/main/java/com/inovaufrpe/makeparty/cliente/gui/FIltroPrecoActivity.java
@@ -0,0 +1,17 @@
+package com.inovaufrpe.makeparty.cliente.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inovaufrpe.makeparty.R;
+
+public class FIltroPrecoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtro_preco);
+    }
+
+
+}
