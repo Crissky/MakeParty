a4688b0457cd0a953837574968748fa023948509
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2019-01-13 09:32:15 -0200

    Correção na GaleriaAdapter e inicialização da novaTelaEscolhaLoginCadastrO(em construção ainda)

--- app/src/main/java/com/inovaufrpe/makeparty/user/gui/adapter/DetalheAnuncioSlideFotos/GaleriaFotosAdapter.java
+++ app/src/main/java/com/inovaufrpe/makeparty/user/gui/adapter/DetalheAnuncioSlideFotos/GaleriaFotosAdapter.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,19 +18,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GaleriaFotosAdapter extends PagerAdapter {
-    //POR ENQUANTO SÓ TA COM 2 FTS E TBM TA COM UMA LISTA BASE - TEM Q RETORNAR A LISTA DAS FOTOS DE VDD AINDA
+    //Quando recebe a lista de imagens em bitmap eu vejo a qtd de itens, se for 0 , coloco um icone sem foto, se n, a qtdFotos = qtdRecebida
     //Aqui se recebe uma lista com array de bitmaps e faz a troca das fotos
-    private Integer[] images = {R.drawable.placeholder, R.drawable.placeholder};
+    //private Integer[] images = {R.drawable.placeholder, R.drawable.placeholder};
     private List<Bitmap> imagens;
     private Context context;
     private LayoutInflater layoutInflater;
+    private int qtdFotos=1;
+
     public GaleriaFotosAdapter(Context context, ArrayList<Bitmap> imagens) {
         this.context = context;
         this.imagens = imagens;
+        if (this.imagens.isEmpty()) {
+            Bitmap iconeSemFoto = BitmapFactory.decodeResource(context.getResources(), R.drawable.placeholder);
+            imagens.add(iconeSemFoto);
+        }
+        this.qtdFotos = imagens.size();
     }
     @Override
     public int getCount() {
-        return images.length;
+        System.out.println(Log.d("QTDFOTOS",String.valueOf(qtdFotos)));
+        return qtdFotos;
     }
     @Override
     public boolean isViewFromObject(View view, Object object) {
@@ -41,14 +50,8 @@ public class GaleriaFotosAdapter extends PagerAdapter {
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View item_view = layoutInflater.inflate(R.layout.viewpager_item, null);
         ImageView imageView = (ImageView) item_view.findViewById(R.id.imageDetalhe);
-        if (this.imagens.isEmpty()){
-            Bitmap iconeSemFoto = BitmapFactory.decodeResource(context.getResources(), R.drawable.placeholder);
-            imageView.setImageBitmap(iconeSemFoto);
-
-        }else{
-            Bitmap imagemBitMap = imagens.get(position);
-            imageView.setImageBitmap(imagemBitMap);
-        }
+        Bitmap imagemBitMap = imagens.get(position);
+        imageView.setImageBitmap(imagemBitMap);
         ViewPager viewPager = (ViewPager) container;
         viewPager.addView(item_view, 0);
         return item_view;
